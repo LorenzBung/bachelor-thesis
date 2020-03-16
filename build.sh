759df20966da4ad5d64e48e4cc2239ef4cc9602d
Lorenz Bung <lorenz.bung@googlemail.com>
2020-03-16 14:00:47 +0100

    Update build file

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 xelatex -synctex=1 -interaction=nonstopmode thesis.tex
+bibtex thesis.aux
+xelatex -synctex=1 -interaction=nonstopmode thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode thesis.tex
