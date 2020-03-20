260953d7ff15658ad0895e9eab89f41ccc6c0458
Lorenz Bung <lorenz.bung@googlemail.com>
2020-03-20 13:31:18 +0100

    Added exit param to latex build file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xelatex -synctex=1 -interaction=nonstopmode thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
 bibtex thesis.aux
-xelatex -synctex=1 -interaction=nonstopmode thesis.tex
-xelatex -synctex=1 -interaction=nonstopmode thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
