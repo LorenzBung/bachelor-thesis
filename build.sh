570725ad944ee941e22f0241568c89e0eb9374cf
Lorenz Bung <lorenz.bung@googlemail.com>
2020-03-20 14:07:54 +0100

    Replace command with latexmk

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
-bibtex thesis.aux
-xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
-xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
+latexmk -xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
