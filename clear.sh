357f4cd6681c73faa7f3fc39599fc7dc0edc2e1c
Lorenz Bung <lorenz.bung@googlemail.com>
2020-03-23 09:38:54 +0100

    Travis runs! Remove debug messages

--- clear.sh
+++ clear.sh
@@ -1,4 +1,4 @@
 # This file will remove any files created during the PDF creation.
 
-rm -rf *.log *.aux *.lof *.lol *.lot *.out *.synctex.gz *.toc *.blg *.bbl
-rm -rf */*.log */*.aux */*.blg */*.bbl
+rm -rf *.log *.aux *.lof *.lol *.lot *.out *.synctex.gz *.toc *.blg *.bbl *.fls *.fdb_latexmk
+rm -rf */*.log */*.aux */*.blg */*.bbl */*.fls */*.fdb_latexmk
