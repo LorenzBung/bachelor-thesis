e4e1e0745b33e191b0340046aba04f25126c5b6c
Lorenz Bung <lorenz.bung@googlemail.com>
2020-04-02 16:24:48 +0200

    Updated a lot of small things

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-latexmk -xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
+latexmk -verbose -xelatex -synctex=1 -interaction=nonstopmode -halt-on-error thesis.tex
